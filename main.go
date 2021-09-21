07b266b8c7fdeffa8f69e317d2e4df93f39da505
David Artov <mr7282@mail.ru>
2021-09-22 00:40:44 +0300

    new commit

--- main.go
+++ main.go
@@ -2,31 +2,24 @@ package main
 
 import (
 	"fmt"
-	// "sort"
 )
-
-// func Counter(s string) string {
-// 	slice := []byte(s)
-// 	return "ok"
-// }
 
 func main()  {
 	slice := []byte("aaabbbccccc")
 
 	type totalItem []byte
 	var total []totalItem
-
+	
 	for iLetter, letter := range slice {
+		var letterCount []byte
 		for _, equal := range slice[iLetter + 1:] {
-			var letterCount []byte
-			if letter == equal {
-				letterCount = append(letterCount, letter)
-				slice = append(slice[:iLetter], slice[iLetter + 1:]...)
-
+			if letter == equal  {
+				letterCount = append(letterCount, equal)
+				slice = append(slice[:iLetter], slice[iLetter + 1:]...) 
 			}
 			fmt.Printf("letterCount: %v\n", letterCount)
-			total = append(total, letterCount)
 		}
+		total = append(total, letterCount)
 	}
 	
 	fmt.Printf("slice: %v\n", total)
